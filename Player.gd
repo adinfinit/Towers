e1b7df3cb6273b97a4bf1ebd87600a23f2951563
Joonatan Samuel <Joonatan.Samuel@starship.xyz>
2017-09-16 16:14:32 +0300

    input map

--- Player.gd
+++ Player.gd
@@ -5,13 +5,13 @@ export var speed = 10.0
 func _fixed_process(delta):
 	var dir = Vector3()
 	
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_forward"):
 		dir += Vector3(0, 0, -1)
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_backwards"):
 		dir += Vector3(0, 0, 1)
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		dir += Vector3(-1, 0, 0)
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		dir += Vector3(1, 0, 0)
 	
 	var t = get_transform()
